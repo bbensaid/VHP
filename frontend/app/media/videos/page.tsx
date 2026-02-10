00e76303483b3e143fb269cb8e1da95d0f307267
bbensaid <bechirbensaid@yahoo.com>
2026-02-09 20:32:18 -0800

    added video library display

---
+++ frontend/app/media/videos/page.tsx
@@ -0,0 +1,31 @@
+import { client } from "@/sanity/lib/client";
+import VideoLibrary from "@/components/VideoLibrary";
+
+export const metadata = {
+  title: "Video Library | HTR",
+  description: "A collection of all video briefings and content from our intelligence platform.",
+};
+
+export const revalidate = 60; 
+
+async function getVideos() {
+  const query = `*[_type == "policyAnalysis" && count(body[_type in ["video", "youtube", "mux.video"]]) > 0] | order(_createdAt desc) {
+    "videos": body[_type in ["video", "youtube", "mux.video"]]{
+      ...,
+      "articleId": ^._id,
+      "articleTitle": ^.title,
+      "articleSlug": ^.slug.current,
+      "pillar": ^.pillar,
+      "category": ^.category 
+    }
+  }`;
+
+  const articlesWithVideos = await client.fetch(query);
+  return articlesWithVideos.flatMap((article) => article.videos);
+}
+
+export default async function VideosPage() {
+  const allVideos = await getVideos();
+
+  return <VideoLibrary allVideos={allVideos} />;
+}
