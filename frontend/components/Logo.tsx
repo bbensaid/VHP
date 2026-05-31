7bc91cf0ba990128c3a70c1d3ab7cd3bd8ebf67e
bbensaid <bechirbensaid@yahoo.com>
2026-05-31 11:42:30 -0700

    Shaping Healthcare Together

--- frontend/components/Logo.tsx
+++ frontend/components/Logo.tsx
@@ -55,7 +55,7 @@ const Logo: React.FC = () => {
         <div className="mt-0 text-center text-[10px] font-medium tracking-wide uppercase flex justify-center items-center gap-1">
           {/* <span className="text-card-economics font-bold">OPTIMIZING CARE</span> */}
     
-          <span className="text-slate-400 font-bold">EVOLVING</span>
+          <span className="text-slate-400 font-bold">SHAPING</span>
           <span className="text-slate-400"></span>
           <span className="text-slate-400 font-bold">HEALTHCARE</span>
           <span className="text-slate-400"></span>
